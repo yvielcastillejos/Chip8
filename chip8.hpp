a525b3f82237c013188bf9e7ac42434bf4863f4a
yvielcastillejos <yviel.castillejos@mail.utoronto.ca>
2020-11-20 01:01:13 -0500

    update

--- chip8.hpp
+++ chip8.hpp
@@ -16,6 +16,14 @@ const unsigned int NUM_REG = 16;
 
 class Chip8{
 	public:
+		Chip8(Display* display);
+		void pwr();
+		// For resetting
+		void Chip8_clear();
+        	void load(const char* path);
+        	void select(uint16_t ins);
+        	void Cycle();
+
 	protected:
 		Display* display;
 		Keypad keypad;
@@ -24,14 +32,30 @@ class Chip8{
 		uint8_t MEMORY[MEM_SIZE];
                 // Stack pointer (8 bits) points to the topmost level of the stack
 		uint8_t SP;
-		// we have 16 16 bit stacks
+		// we have 16 16 bit stacks for storing the addr the interpreter should return to when finished running a function
 		uint16_t Stack[STACK_SIZE];
 
 		// Register
-		uint8_t V[NUM_REGS];
+		uint8_t V[NUM_REG];
 		// I is 16 bit register used to store memory addr
 		uint16_t I;
+		
+		// Program counter - 16 bit used to store the currently executing address
+		uint16_t PC;
+		unsighed short opcode;
+		
 
+		// Timers Registers
+		uint8_t delay_timer;
+		uint8_t sound_timer;
 
+		
+		bool PGR_loaded;
+	//	bool waitingForInput;
+	//	bool updateDisplay;
+		unsigned accumDelta; //Accums delta time between frames, for timers update
 
+		//stack helpers
+		void push(uint16_t v);
+		void pop();  
 		}
